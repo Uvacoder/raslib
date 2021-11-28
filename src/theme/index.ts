da0930255c9b8bb6d953be8483a831a364556352
Lesley Chang <lesley.hanna98@gmail.com>
2021-11-28 13:13:29 -0500

    use barrel import and move theme declaration

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,11 +1,5 @@
-import { radii } from './border'
-import { brand, palette } from './colors'
-import { fontSizes, fontWeights } from './typography'
-
-export const theme = {
-  palette,
-  brand,
-  fontWeights,
-  fontSizes,
-  radii
-}
+export * from './border'
+export * from './color.utils'
+export * from './colors'
+export * from './theme'
+export * from './typography'
---
+++ src/theme/theme.ts
@@ -0,0 +1,11 @@
+import { radii } from './border'
+import { brand, palette } from './colors'
+import { fontSizes, fontWeights } from './typography'
+
+export const theme = {
+  palette,
+  brand,
+  fontWeights,
+  fontSizes,
+  radii
+}
