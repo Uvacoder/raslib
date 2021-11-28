5bcddc3bfa46b0911ddf92f378ee3132b5e91b21
Lesley Chang <lesley.hanna98@gmail.com>
2021-11-28 12:50:52 -0500

    update ignores

--- src/theme/color.utils.ts
+++ src/theme/color.utils.ts
@@ -1,22 +1,20 @@
-import {
-  palette
-} from "./colors"
+import { palette } from './colors'
 
 export type ColorKey = keyof typeof palette
 
 export type ColorLevel = keyof typeof palette[ColorKey]
 
 const getColorConfigPath = (key: ColorKey, level: ColorLevel) => {
-  return `${key}.${level}` 
+  return `${key}.${level}`
 }
 
-export type ColorConfigPath = ReturnType < typeof getColorConfigPath >
+export type ColorConfigPath = ReturnType<typeof getColorConfigPath>
 
-  export const getColor = (path: ColorConfigPath) => {
-    const splits = path.split('.')
-    const key = splits.at(0)!
-      // eslint-disable-next-line radix
-      const level = parseInt(splits.at(1) !)
+export const getColor = (path: ColorConfigPath) => {
+  const splits = path.split('.')
+  const key = splits.at(0)!
+  // eslint-disable-next-line radix
+  const level = parseInt(splits.at(1)!)
 
-    return palette[key as ColorKey][level as ColorLevel]
-  }
+  return palette[key as ColorKey][level as ColorLevel]
+}
