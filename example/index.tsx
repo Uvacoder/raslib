06c6e64d0cc430280b58a6082772f5dd15134237
Lesley Chang <lesley.hanna98@gmail.com>
2021-11-28 12:13:37 -0500

    theme utils

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,13 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { getColor } from '../.';
 
 const App = () => {
   return (
     <div>
-      <Thing />
+HI
+<p>Random color: {getColor('gray.600')}</p>
     </div>
   );
 };
--- src/theme/color.utils.ts
+++ src/theme/color.utils.ts
@@ -1,20 +1,22 @@
-import {palette} from "./colors"
+import {
+  palette
+} from "./colors"
 
 export type ColorKey = keyof typeof palette
 
 export type ColorLevel = keyof typeof palette[ColorKey]
 
 const getColorConfigPath = (key: ColorKey, level: ColorLevel) => {
-  return `${key}.${level}` as const
+  return `${key}.${level}` 
 }
 
-export type ColorConfigPath = ReturnType<typeof getColorConfigPath>
+export type ColorConfigPath = ReturnType < typeof getColorConfigPath >
 
-export const getColor = (path: ColorConfigPath) => {
-  const splits = path.split('.')
-  const key = splits.at(0)! as ColorKey
-  // eslint-disable-next-line radix
-  const level = parseInt(splits.at(1)!) as ColorLevel
+  export const getColor = (path: ColorConfigPath) => {
+    const splits = path.split('.')
+    const key = splits.at(0)!
+      // eslint-disable-next-line radix
+      const level = parseInt(splits.at(1) !)
 
-  return palette[key][level]
-}
+    return palette[key as ColorKey][level as ColorLevel]
+  }
