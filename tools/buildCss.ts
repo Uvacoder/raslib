14bb79c9820650ccc67afb9d37b7a215c4ae550d
Lesley Chang <lesley.hanna98@gmail.com>
2021-11-28 12:52:44 -0500

    asdf

--- tools/buildCss.ts
+++
@@ -1,7 +0,0 @@
-import {buildCssUtilities} from "./buildCssUtilities"
-import {buildCssVars} from "./buildCssVars"
-
-export const buildCss = (theme = {palette: {}, brand:{}, fontSizes: {}, fontWeights: {},radii:{}}) => {
-	buildCssVars(theme)
-	buildCssUtilities(theme)
-}
