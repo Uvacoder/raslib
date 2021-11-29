c40187f3b8e050518f888dff1d0cd10d523b9b41
Lesley Chang <lesley.hanna98@gmail.com>
2021-11-28 19:42:50 -0500

    feat: finally got install working externally

--- example/index.tsx
+++ example/index.tsx
@@ -1,17 +1,17 @@
-import 'react-app-polyfill/ie11';
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { getColor } from '../.';
+import 'react-app-polyfill/ie11'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import { getColor } from '@rasreee/theme'
 
-import "../css/index.css"
+import '../css/index.css'
 
 const App = () => {
   return (
-    <div style={{color: 'var(--color-green-500)', fontWeight: 'var(--fontWeight-extrabold)'}}>
-HI
-<p>Random color: {getColor('gray.600')}</p>
+    <div style={{ color: 'var(--color-green-500)', fontWeight: 'var(--fontWeight-extrabold)' }}>
+      HI
+      <p>Random color: {getColor('gray.600')}</p>
     </div>
-  );
-};
+  )
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'))
